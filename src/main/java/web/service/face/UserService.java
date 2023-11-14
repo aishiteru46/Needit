0a6963646637228fb93a6025e5dbcc7f2d5cea39
Tanasire <Tanasire@DESKTOP-B62N5BL>
2023-11-14 10:26:03 +0900

    231114_1024_LJB 이중배 로그인 회원가입 작업

--- src/main/java/web/service/face/UserService.java
+++ src/main/java/web/service/face/UserService.java
@@ -6,10 +6,21 @@ import web.dto.User;
 
 @Service
 public interface UserService {
+	
+	//회원가입
 	public void join(User user);
 
+	//로그인
 	public boolean login(User user);
 
+	//닉네임 정보
 	public User infoNick(User user);
+
+	//아이디 체크
+	public boolean idCheck(User user);
+
+	//닉네임 체크
+	public boolean ncikCheck(User user);
+
 
 }
