cec4b7ad1dbfd2858159292a68b81d71982233ef
rudgn <rudgn@gh5340>
2023-11-17 14:05:15 +0900

    231117_1404_pgh_알림 MVC + 관련 pom.xml(org.json) +jsp 업로드 아직 안합침

---
+++ src/main/java/web/service/face/AlertService.java
@@ -0,0 +1,44 @@
+package web.service.face;
+
+import java.util.List;
+
+import web.dto.Alert;
+
+public interface AlertService {
+	
+	/**
+	 * Alert객체에 저장된 id로 조회해
+	 * 해당 유저가 안 읽은 알림의 갯수를 불러온다
+	 * 
+	 * @param alert Alert 객체 
+	 * @return 안읽은 알람의 수
+	 */
+	public int hasNew(Alert alert);
+
+	/**
+	 * 유저에게 온 모든 알림을 Alert 객체에 저장된
+	 * 유저의 id를 이용해  list로 불러온다
+	 * 
+	 * @param alert Alert 객체
+	 * @return Alert List 
+	 */
+	public List<Alert> getList(Alert alert);
+	
+	/**
+	 * 알림의 읽음 여부를 받아와
+	 * 알림의 읽음 여부를 변경한다
+	 * 0 : 읽지 않음, 1 : 읽음
+	 * 
+	 * @param alert
+	 */
+	public void readAlert(Alert alert);
+	
+	/**
+	 * 새로 받아온 알림을 DB에 저장한다
+	 * 
+	 * @param alert Alert 객체
+	 */
+	public void sendAlert(Alert alert);
+	
+	
+}
---
+++ src/main/java/web/service/impl/AlertServiceImpl.java
@@ -0,0 +1,38 @@
+package web.service.impl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import web.dao.face.AlertDao;
+import web.dto.Alert;
+import web.service.face.AlertService;
+
+@Service
+public class AlertServiceImpl implements AlertService {
+	@Autowired AlertDao alertDao;
+
+	@Override
+	public int hasNew(Alert alert) {
+		return alertDao.countStatus(alert);
+	}
+
+	@Override
+	public List<Alert> getList(Alert alert) {
+		return alertDao.selectByUser(alert);
+	}
+
+	@Override
+	public void readAlert(Alert alert) {
+		alertDao.updateAlertStatus(alert);
+	}
+
+	@Override
+	public void sendAlert(Alert alert) {
+		alertDao.insertAlert(alert);
+	}
+
+}
